707f781ef8079c0a3f4373802797defc40ce5aac
SakshiWalzade <sakshiwalzade79@gmail.com>
2025-02-18 16:40:32 +0530

    develop added

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
         System.out.printf("  welcome!");
         System.out.printf("  yyyy!");
 
+        System.out.printf(" Develop!");
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
